92e68362cf11e4e84571161c908b8a29eec5759b
David <davidongaroh@gmail.com>
2024-05-31 19:53:14 +0300

    create actions

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,41 @@
 class CocktailsController < ApplicationController
-end
+    def index
+      @cocktails = Cocktail.all
+      render json: @cocktails
+    end
+  
+    def show
+      @cocktail = Cocktail.find(params[:id])
+      render json: @cocktail
+    end
+  
+    def create
+      @cocktail = Cocktail.new(cocktail_params)
+      if @cocktail.save
+        render json: @cocktail, status: :created
+      else
+        render json: @cocktail.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      @cocktail = Cocktail.find(params[:id])
+      if @cocktail.update(cocktail_params)
+        render json: @cocktail
+      else
+        render json: @cocktail.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @cocktail = Cocktail.find(params[:id])
+      @cocktail.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def cocktail_params
+      params.require(:cocktail).permit(:name, :description, :ingredients, :instructions, :image)
+    end
+  end
