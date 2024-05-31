6998cd0d28b8f37721993502075e9f76e98c4f7e
David <davidongaroh@gmail.com>
2024-05-31 19:50:20 +0300

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :cocktails, only: [:index, :show, :create, :update, :destroy]
 end
